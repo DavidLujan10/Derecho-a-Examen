6e8d69f2697a7de351c4c69cc33bf787283836fe
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-11-11 15:04:03 -0600

    PRIMITIVAS GIRANDO CON LA PLATAFORMA ABAJO

--- Primitivas/www/js/scene.js
+++ Primitivas/www/js/scene.js
@@ -7,50 +7,74 @@
         renderer.setSize(window.innerWidth, window.innerHeight);//TAMAÑO AL RENDERER OSEA ANCHO Y ALTO DE LA ESCENA Y RENDER ES DIBUJAR LO QUE SE ESTA CREANDO
         document.body.appendChild(renderer.domElement);
 
+        renderer.shadowMap.enabled = true;//DAR DE ALTA EL SERVICIO DE LAS SOBRAS PARA PODER USARLAS
+        renderer.shadowMap.soft = true;//LA SOMBRA MAS SUAVE CON RESPECTO LA LUZ QUE ESTOY PROYECTANDO
+        renderer.shadowMap.type = THREE.PCFShadowMap;//TIPO DE SOMBRA QUE VAMOS A USAR
+
         camera.position.z = 20;//PROFUNDIDAD CON LA QUE SE VIZUALISARA LA CAMARA
-        camera.position.y = 1;//ALTURA DE LA CAMARA
+        camera.position.y = 5;//ALTURA DE LA CAMARA
         //camera.position.x = 0;//LARGO DE LA CAMARA    
+
+    
+        //la plataforma gris 
+        let planeGeometry = new THREE.PlaneGeometry(200,1500);// creacion del plano y su tamano 
+        planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));// matrix de 4 ejes
+        //  se rota y se refresque constantemente 
+        let groundMaterial = new THREE.MeshPhongMaterial({// genera el material en el mesh
+            color: 0xffffff
+        });
+        let plane = new THREE.Mesh(planeGeometry, groundMaterial);// crear la geometria del plano
+        plane.receiveShadow  = true;//QUIEN VA A RECBIR LA SOMBRA
+        let loader = new THREE.TextureLoader();
+
+
     
     
         let piramideGeometria = new THREE.ConeGeometry( 5, 10, 50, 50, false);//1ATRIBUTO ES EL RADIO DE LA PARTE BASE (DEFAULT=20), 2ATRIBUTO ES LA ALTURA DEL CONO (DEFAULT=100), 3ATRIBUTO ES EL NO. DE FIGURAS QUE CONFORMA LA BASE DEL CONO, 4ATRIBUTO ES EL NUMERO DE FIGURAS QUE CONFORMA LA ALTURA DEL CONO, 5ATRIBUTO INDICA SI ESTA TAPADA LA BASE DEL CONO (DEFAULT=FALSE ES DECIR TIENE "CARA"), 6ATRIBUTO ES EL ANGULO INICIAL (DEFAULT=0), 7ATRIBUTO ES EL ANGULO CENTRAL (DEFAULT=2*Pi)
         let cuboGeometria = new THREE.BoxGeometry(2, 2, 2, 2);//SE DEFINE EL TIMPO DE FIGURA GEOMETRIA (EN ESTE CASA UN CUBO)
-        let donaGeometria = new THREE.RingGeometry(3, 5, 50, 1);//1ATRIBUTO RADIO DEL ANILLO INTERIOR (DEFAULT=20), 2ATRIBUTO EL RADIO EXTERIOR (DEFAULT=50), 3ATRIBUTO ES EL NUMERO DE FIGURAS QUE CONFORMAN EL ANILLO, 4ATRIBUTO ES EL phiSegments(minimo 1), 5ATRIBUTO ES EL ANGULO INICIAL (DEFAULT=0) Y EL 6ATRIBUTO ES EL ANGULO CENTRAL (DEFAULT=2*Pi)
+        let donaGeometria = new THREE.TorusGeometry(3, 2, 16, 100);
         
         let materialPiramide = new THREE.MeshPhongMaterial({ //DECLARA MATERIAL CON EL QUE SE RELLENA LA FIGURA ACEPTA PROPIEDADES (PhongMaterial)
-            color: 0xffffff
+            color: 0x00D4FF
         });
 
         let materialCubo = new THREE.MeshPhongMaterial({
-            color: 0xfffff,
+            color: 0xFF0000
         });
 
         let materialDona = new THREE.MeshPhongMaterial({
-            color: 0xffff00
+            color: 0xffff00, 
         });
         
         
         let piramide = new THREE.Mesh(piramideGeometria, materialPiramide);//GENERAMOS UNA REJILLA (mesh) DONDE GUARDAMOS LAS PROPIEDADES DE LA GEOMETRIA Y DEL MATERIAL DE LA FIGURA 
         piramide.position.x = 20;
+        piramide.position.y = 8;
 
         let cubo = new THREE.Mesh(cuboGeometria, materialCubo );
         cubo.position.x = -20;
+        cubo.position.y = 5;
  
         let dona = new THREE.Mesh(donaGeometria, materialDona );
-        dona.position.y = 5;
+        dona.position.y = 6;
         
     
         let puntoLuz = new THREE.PointLight(0x404040);//CREAMOS UNA LUZ DE TIPO PUNTO
         puntoLuz.position.y = 80;//POSICION (EN EL EJE) DE LA LUZ 
-        //puntoLuz.position.z = 20;//POSICION (EN EL EJE) DE LA LUZ 
+        puntoLuz.position.z = 20;//POSICION (EN EL EJE) DE LA LUZ 
     
-    
+        puntoLuz.castShadow = true;//PARA QUE LA LUZ(PUNTUAL) PROYECTE LA SOMBRA
+
         scene.add(piramide);//AGREGAMOS LA REGILLA (mesh) A LA ESCENA 
         scene.add(cubo);
         scene.add(dona);
         scene.background = new THREE.Color(0xeeeeee);
         scene.add(new THREE.AmbientLight(0x404040));//AGREGAMOS OTRA PEQUEÑA LUZ (luz de ambiente)
         scene.add(puntoLuz);//AGREGAMOS EL PUNTO LUZ A LA ESCENA
+        scene.add(plane);
+            
         
+        let controls = new THREE.OrbitControls(camera, renderer.domElement);//ESTO ES PARA PODER MOVER LA CAMARA POR LA ESCENA
     
     
         function loop(){//CICLA CONSTANTEMENTE LOS FRAMES QUE VAS A GENERAR
