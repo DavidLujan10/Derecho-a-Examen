55d518c3149408262b34a1deaad5e3cdfaac0d8c
luisfayre <luisfayre@hotmail.com>
2017-11-11 19:10:11 -0700

    AmbientLight + Shadow
    AmbientLight + Shadow

--- Primitivas/www/js/scene.js
+++ Primitivas/www/js/scene.js
@@ -1,7 +1,7 @@
 (function () { //FLOUSURE: FUNCION ANONIMA QUE SE LLAMA A SI MISMA
 
     let scene = new THREE.Scene(); //Creacion de la esena
-    const aspectRatio = window.innerWidth / window.innerHeight; 
+    const aspectRatio = window.innerWidth / window.innerHeight;
     let camera = new THREE.PerspectiveCamera(85, aspectRatio, 0.1, 100); //Perspectiva de la camara
     let renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -9,6 +9,10 @@
     document.body.appendChild(renderer.domElement);
 
     var controls = new THREE.TrackballControls(camera);
+    controls.maxPolarAngle = Math.PI * 0.5;
+    controls.minDistance = 10;
+    controls.maxDistance = 75;
+    controls.target.set( 0, 2.5, 0 );
 
     renderer.shadowMap.enabled = true;//DAR DE ALTA EL SERVICIO DE LAS SOBRAS PARA PODER USARLAS
     renderer.shadowMap.soft = true;//LA SOMBRA MAS SUAVE CON RESPECTO LA LUZ QUE ESTOY PROYECTANDO
@@ -18,41 +22,42 @@
     camera.position.y = 5; //ALTURA DE LA CAMARA
 
     //Formas
-    var piramideGeometria = new THREE.ConeGeometry(5, 10, 50, 50, false);
+    var piramideGeometria = new THREE.ConeGeometry(5, 10, 4, 1, false);
     var cuboGeometria = new THREE.BoxGeometry(2, 2, 2, 2);
     var donaGeometria = new THREE.TorusGeometry(3, 2, 16, 100);
     let planeGeometry = new THREE.PlaneGeometry(200, 900); //Creacion del plano y su tamano 
     planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2)); //Matrix de 4 ejes
+
     //Propiedades de las formas
     var piramide = new THREE.Mesh(piramideGeometria, color());
     var cubo = new THREE.Mesh(cuboGeometria, color());
     var dona = new THREE.Mesh(donaGeometria, color());
     let groundMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
     let plane = new THREE.Mesh(planeGeometry, groundMaterial);
-    plane.receiveShadow = true; //QUIEN VA A RECBIR LA SOMBRA
+
+    //Sombra
+    piramide.castShadow = true;
+    cubo.castShadow = true;
+    dona.castShadow = true;
+    plane.receiveShadow = true;
 
     //Posicionamiento de las formas
     piramide.position.x = 20;
     piramide.position.y = 8;
-
     cubo.position.x = -20;
     cubo.position.y = 5;
-
     dona.position.y = 6;
+
+    let puntoLuz = new THREE.PointLight(0xdfebff, .9);
+    puntoLuz.position.set(50, 80, 20);
+    puntoLuz.castShadow = true;
+
+    scene.add(new THREE.AmbientLight('#DFFBFF'));
 
     scene.add(piramide);
     scene.add(cubo);
     scene.add(dona);
     scene.add(plane);
-
-    //Posicionamiento de la luz
-    let puntoLuz = new THREE.PointLight(0x404040);
-    puntoLuz.position.y = 80;
-    puntoLuz.position.z = 20;
-
-    puntoLuz.castShadow = true;
-    scene.background = new THREE.Color(0xeeeeee);
-    scene.add(new THREE.AmbientLight(0x404040));
     scene.add(puntoLuz);
 
     function color() { //Funcion para el cambio de material con las formas
@@ -61,6 +66,7 @@
         });
         return groundMaterialFormas;
     }
+
 
     function loop() {
         controls.update(); //Actualizacion de los controles de camara
